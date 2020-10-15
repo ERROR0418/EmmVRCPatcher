00853505295af86208b83be853768875affb8b63
ERROR#0418 <72815002+ERROR0418@users.noreply.github.com>
2020-10-15 21:34:09 +0100

    Update Patcher.cs

--- EmmVRCCustom/Patcher.cs
+++ EmmVRCCustom/Patcher.cs
@@ -1,28 +1,42 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Reflection;
 using emmVRC;
+using emmVRC.Libraries;
 using emmVRC.Managers;
 using emmVRC.Menus;
 using emmVRC.Network;
+using EmmVRCCustom;
+using Harmony;
 using MelonLoader;
 using UnityEngine.UI;
+using VRC.Core;
 
 namespace emmVRCCustom
 {
 	// Token: 0x0200000B RID: 11
 	public static class Patcher
 	{
-		// Token: 0x0600003E RID: 62 RVA: 0x00002FCC File Offset: 0x000011CC
-		public static void PatchEmm()
+
+		public static void HideButton()
 		{
-			MelonLogger.Log("Patching EMMVRC URL's -- ERROR#0418");
+			//MelonLogger.Log("Hiding button ;)");
+			UnityEngine.Object.Destroy(QuickMenuUtils.GetQuickMenuInstance().transform.Find("ShortcutMenu/ReportWorldButton").gameObject);
+			FunctionsMenu.baseMenu.menuName = "Report World";
+			FunctionsMenu.baseMenu.menuEntryButton.setButtonText("Report\nWorld");
+			FunctionsMenu.baseMenu.menuEntryButton.setToolTip("Report Issues with this World");
+			FunctionsMenu.baseMenu.menuEntryButton.setLocation(1, -1);
+		}
+
+        public static void PatchURL()
+        {
+			MelonLogger.Log("Patching baseURL");
 			try
 			{
-				FieldInfo field = typeof(NetworkClient).GetField("BaseAddress", BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
-				field.SetValue(typeof(NetworkClient), "http://127.0.0.1");
-				MelonLogger.Log("Base Address: " + field.GetValue(null));
-				MelonLogger.Log("Base URL: " + NetworkClient.baseURL);
-				MelonLogger.Log("Patching Complete!");
+				HarmonyInstance harmony = HarmonyInstance.Create("CustomPatch.baseURL");
+				MethodInfo original = typeof(emmVRC.Network.NetworkClient).GetMethod("get_baseURL");
+				MethodInfo patch = typeof(HarmonyPatches).GetMethod("get_baseURL");
+				harmony.Patch(original, prefix: new HarmonyMethod(patch));
 			}
 			catch (Exception)
 			{
@@ -30,30 +44,33 @@ namespace emmVRCCustom
 			}
 		}
 
+        // Token: 0x0600003E RID: 62 RVA: 0x00002FCC File Offset: 0x000011CC
+        public static void PatchEmm()
+		{
+			MelonLogger.Log("Patching AvatarProcess()");
+            try
+            {
+				HarmonyInstance harmony = HarmonyInstance.Create("CustomPatch.Avatars");
+				MethodInfo original = typeof(emmVRC.Managers.AvatarPermissionManager).GetMethod("ProcessAvatar");
+				MethodInfo patch = typeof(HarmonyPatches).GetMethod("ProcessAvatar");
+				harmony.Patch(original, postfix: new HarmonyMethod(patch));
+			}
+			catch(Exception ex)
+            {
+				MelonLogger.LogError(ex.ToString());
+            }
+		}
+
 		// Token: 0x0600003F RID: 63 RVA: 0x00003064 File Offset: 0x00001264
 		public static void PatchRiskyFuncs()
 		{
+			MelonLogger.Log("Patching SetRiskyFunctions()");
 			try
 			{
-				if (!RiskyFunctionsManager.RiskyFunctionsAllowed)
-				{
-					MelonLogger.Log("Patching EMMVRC RiskyFuncs -- ERROR#0418");
-					Configuration.JSONConfig.RiskyFunctionsEnabled = false;
-					MelonLogger.Log("Have stopped checks? maybe?");
-					RiskyFunctionsManager.RiskyFunctionsChecked = true;
-					MelonLogger.Log("Risky funcs are " + (RiskyFunctionsManager.RiskyFunctionsAllowed ? "Allowed" : "Denied"));
-					MelonLogger.Log("Patching... ");
-					typeof(RiskyFunctionsManager).GetField("<RiskyFunctionsAllowed>k__BackingField", BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic).SetValue(typeof(RiskyFunctionsManager), true);
-					MelonLogger.Log("Risky funcs are now " + (RiskyFunctionsManager.RiskyFunctionsAllowed ? "Allowed" : "Denied"));
-					PlayerTweaksMenu.WaypointMenu.getGameObject().GetComponent<Button>().enabled = true;
-					PlayerTweaksMenu.FlightToggle.getGameObject().GetComponent<Button>().enabled = true;
-					PlayerTweaksMenu.NoclipToggle.getGameObject().GetComponent<Button>().enabled = true;
-					PlayerTweaksMenu.SpeedToggle.getGameObject().GetComponent<Button>().enabled = true;
-					PlayerTweaksMenu.ESPToggle.getGameObject().GetComponent<Button>().enabled = true;
-					PlayerTweaksMenu.SpeedSlider.slider.GetComponent<Slider>().enabled = true;
-					PlayerTweaksMenu.SpeedText.GetComponent<Text>().text = "Speed: Disabled";
-					PlayerTweaksMenu.EnableJumpButton.getGameObject().GetComponent<Button>().enabled = true;
-				}
+				HarmonyInstance harmony = HarmonyInstance.Create("CustomPatch.Risky");
+				MethodInfo original = typeof(emmVRC.Menus.PlayerTweaksMenu).GetMethod("SetRiskyFunctions");
+				MethodInfo patch = typeof(HarmonyPatches).GetMethod("SetRiskyFunctions");
+				harmony.Patch(original, prefix: new HarmonyMethod(patch));
 			}
 			catch (Exception)
 			{
